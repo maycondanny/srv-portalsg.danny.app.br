1a0628092939cf1004d9c11f8bd420b98a0054d9
Mateus Deli <mateus_forlevesi@hotmail.com>
2024-10-24 15:39:19 -0300

    feat: implementacao modulos e roles

---
+++ src/db/migrations/20241024183853_DROP_TABLE_acessos_grupos.ts
@@ -0,0 +1,7 @@
+import type { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  return knex.schema.dropTableIfExists('acessos_grupos');
+}
+
+export async function down(knex: Knex): Promise<void> {}
